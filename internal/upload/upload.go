55716073bf6f01b59771f9eba3de2262dbb537d2
krisjanisp <krisjanispetrucena@gmail.com>
2024-03-08 13:46:04 +0200

    inserting a new task version

--- internal/upload/upload.go
+++ internal/upload/upload.go
@@ -2,8 +2,12 @@ package upload
 
 import (
 	"log"
+	"math"
 
+	"github.com/go-jet/jet/v2/postgres"
+	"github.com/go-jet/jet/v2/qrm"
 	"github.com/jmoiron/sqlx"
+	"github.com/programme-lv/uploader/internal/database/proglv/public/model"
 	"github.com/programme-lv/uploader/internal/database/proglv/public/table"
 )
 
@@ -15,13 +19,19 @@ func UploadTask(taskDir string, uploader S3Uploader, sqlxDB *sqlx.DB) error {
 	}
 	log.Println(problem)
 
-	taskID, err := createANewTask(problem, sqlxDB)
+	taskID, err := createTask(sqlxDB)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	log.Println(taskID)
+	log.Println("TaskID:", taskID)
 
+	versionID, err := createTaskVersion(sqlxDB, taskID, problem)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("VersionID:", versionID)
 	// create a new task
 	// create a new task version
 	// fill task_version_tests, text_files
@@ -34,28 +44,56 @@ func UploadTask(taskDir string, uploader S3Uploader, sqlxDB *sqlx.DB) error {
 	return nil
 }
 
-func createANewTask(problem Problem, sqlxDB *sqlx.DB) (int, error) {
+func createTaskVersion(db qrm.Queryable, taskID int, problem Problem) (int, error) {
+	insertStmt := table.TaskVersions.INSERT(
+		table.TaskVersions.TaskID,
+		table.TaskVersions.ShortCode,
+		table.TaskVersions.FullName,
+		table.TaskVersions.TimeLimMs,
+		table.TaskVersions.MemLimKibibytes,
+		table.TaskVersions.TestingTypeID,
+		// table.TaskVersions.Origin,
+		table.TaskVersions.CreatedAt,
+		// table.TaskVersions.CheckerID,
+		// table.TaskVersions.InteractorID,
+	).VALUES(
+		taskID,
+		problem.Code,
+		problem.Name,
+		math.Round(problem.Time*1000),
+		problem.Memory*1024,
+		problem.Type,
+		postgres.NOW(),
+	).RETURNING(table.TaskVersions.ID)
+
+	dest := model.TaskVersions{}
+	err := insertStmt.Query(db, &dest)
+	if err != nil {
+		return 0, err
+	}
+
+	return int(dest.ID), nil
+}
+
+func createTask(db qrm.Queryable) (int, error) {
 	insertStmt := table.Tasks.INSERT(
 		table.Tasks.CreatedAt,
 		table.Tasks.CreatedByID,
 		table.Tasks.RelevantVersionID,
 		table.Tasks.PublishedVersionID,
 	).VALUES(
-		"now()",
+		postgres.NOW(),
 		1,
 		nil,
 		nil,
 	).RETURNING(table.Tasks.ID)
 
-	dest := struct {
-		ID int `db:"id"`
-	}{}
-
-	err := insertStmt.Query(sqlxDB.DB, &dest)
+	dest := model.Tasks{}
+	err := insertStmt.Query(db, &dest)
 	if err != nil {
 		return 0, err
 	}
 
-	return dest.ID, nil
+	return int(dest.ID), nil
 
 }
